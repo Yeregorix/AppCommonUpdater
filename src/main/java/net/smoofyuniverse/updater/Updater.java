b5f7cd3f3a639c8232b1b1efe5cf746df0903b10
Yeregorix <yeregorixcraft@gmail.com>
2019-03-13 07:22:58 +0100

    Replace toLowerCase() with toLowerCase(Locale.ROOT)

--- src/main/java/net/smoofyuniverse/updater/Updater.java
+++ src/main/java/net/smoofyuniverse/updater/Updater.java
@@ -30,6 +30,7 @@ import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class Updater {
 	public static final int MINIMUM_VERSION = 1;
@@ -45,7 +46,7 @@ public class Updater {
 		if (args.length < 4)
 			throw new IllegalArgumentException("length");
 
-		update(Paths.get(args[1]), Paths.get(args[2]), args[3].toLowerCase().equals("true"), Arrays.copyOfRange(args, 4, args.length));
+		update(Paths.get(args[1]), Paths.get(args[2]), args[3].toLowerCase(Locale.ROOT).equals("true"), Arrays.copyOfRange(args, 4, args.length));
 	}
 
 	public static void update(Path source, Path target, boolean launch, String[] args) throws IOException, InterruptedException {
